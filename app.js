75d3c9f53c8a876695e0dbc5affb2c8074e99b3a
Abdul Ahad <ahada00077@gmail.com>
2024-10-14 23:40:53 +0500

    new animation

---
+++ app.js
@@ -0,0 +1,39 @@
+document.addEventListener(
+    "DOMContentLoaded",
+    function() {
+      setTimeout(function() {    document.getElementById("loading").className = "slideDown";
+      }, 3800);
+    },
+    false
+  );
+  
+  document.addEventListener(
+    "DOMContentLoaded",
+    function() {
+      setTimeout(function() {
+        document.getElementById("loading-center").className = "zoomOut";
+      }, 3200);
+    },
+    false
+  );
+  
+  
+  document.addEventListener(
+    "DOMContentLoaded",
+    function() {
+      setTimeout(function() {
+        document.getElementById("block-1").className = "slideInUp";
+      }, 3800);
+    },
+    false
+  );
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
