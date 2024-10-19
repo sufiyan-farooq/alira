38f5f9527f1f0c275b85beafd45c79ee741d9459
Abdul Ahad <ahada00077@gmail.com>
2024-10-20 00:34:31 +0500

    New Chnge

--- app.js
+++ app.js
@@ -27,6 +27,23 @@ document.addEventListener(
     },
     false
   );
+  // fade in grid items  ==================================
+
+  $(document).on("scroll", function () {
+    var pageTop = $(document).scrollTop()
+    var pageBottom = pageTop + $(window).height()
+    var tags = $(".fadein")
+
+    for (var i = 0; i < tags.length; i++) {
+      var tag = tags[i]
+
+      if ($(tag).offset().top < pageBottom) {
+        $(tag).addClass("visible")
+      } else {
+        $(tag).removeClass("visible")
+      }
+    }
+  })
   
   
   
